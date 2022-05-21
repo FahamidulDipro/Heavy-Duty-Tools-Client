cc1c9704b8b8ed6198a33abc76419d9134bad104
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-05-22 00:32:58 +0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Pages/Home';
+import Navigation from './components/Shared/Navigation';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation></Navigation>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
+
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        website_theme: {
+          primary: "#fdb819",
+          secondary: "#212121",
+          accent: "#ffa800",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+
+}
